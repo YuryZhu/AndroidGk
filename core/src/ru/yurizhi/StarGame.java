5839d946c4a1147faa017c43fec44421e73d7d60
yury <zhurikk@ya.ru>
2019-01-18 20:17:57 +0300

    first lesson

--- core/src/ru/yurizhi/StarGame.java
+++ core/src/ru/yurizhi/StarGame.java
@@ -5,15 +5,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
+	private TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bat.jpg");
+		img = new Texture("justice_league.png");
+		region = new TextureRegion(img, 500, 0, 150, 140);
 	}
 
 	@Override
@@ -21,7 +26,8 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
+		batch.draw(region, 0, 0);
 		batch.end();
 	}
 	
