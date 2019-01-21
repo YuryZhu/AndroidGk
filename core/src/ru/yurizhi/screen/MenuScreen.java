930dd06c9255946d38d41a46fa46f9b82ba977fc
yury <zhurikk@ya.ru>
2019-01-21 23:00:21 +0300

    les3

--- core/src/ru/yurizhi/screen/MenuScreen.java
+++ core/src/ru/yurizhi/screen/MenuScreen.java
@@ -2,18 +2,17 @@ package ru.yurizhi.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import ru.yurizhi.base.Base2DScreen;
 
-import javax.swing.text.Position;
-
 
 public class MenuScreen extends Base2DScreen {
 
-    SpriteBatch batch;
+    private static final float V_LEN = 2.5f;
+
     Texture img;
     Texture background;
     TextureRegion region;
@@ -27,33 +26,29 @@ public class MenuScreen extends Base2DScreen {
 
     Vector2 pos;
     Vector2 v;
-    boolean move;
-    Vector2 speed2, moveTo;
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         background = new Texture("bat.jpg");
         img = new Texture("justice_league.png");
         region = new TextureRegion(img, 500, 0, 160, 170);
-        pos = new Vector2(0, 0);
-        v = new Vector2(0, 0);
+        pos = new Vector2(-0.5f, -0.5f);
+        v = new Vector2(0.002f, 0.002f);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        Gdx.gl.glClearColor(0.5f, 0.2f, 0.3f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(region, pos.x, pos.y);
-        updateMotion();
-        pos.add(v);
-        if (pos.x < 0) pos.x = 0;
-        if (pos.x > 1280 - 155) pos.x = 1280 - 155;
-        if (pos.y < 0) pos.y = 0;
-        if (pos.y > 720 - 160) pos.y = 720 - 160;
+        batch.draw(background, -0.5f, -0.5f, 1f, 1f);
+        batch.draw(region, pos.x, pos.y, 0.2f, 0.2f);
         batch.end();
+        updateMotion();
+//        pos.add(v);
+
     }
 
     @Override
@@ -63,29 +58,16 @@ public class MenuScreen extends Base2DScreen {
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchDown " + screenX + " " + (Gdx.graphics.getHeight() - screenY));
-        int y = (Gdx.graphics.getHeight() - screenY - 75);
-        int x = screenX - 75;
-//        pos.set(x, y);
-        int tempX = x;
-        int tempY = y;
-        double distance;
-        float time = 500 * Gdx.graphics.getDeltaTime();
-        distance = Math.sqrt((tempX - pos.x) * (tempX - pos.x) + (tempY - pos.y) * (tempY - pos.y));
-        v.x += 0.1 * time * (tempX - pos.x) / distance;
-        v.y += 0.1 * time * (tempY - pos.y) / distance;
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return super.touchDown(touch, pointer);
     }
-
     public void updateMotion() {
-        speed = 500 * Gdx.graphics.getDeltaTime();
+        speed = (float) (0.3 * Gdx.graphics.getDeltaTime());
         if (leftMove) {
             pos.x -= speed;
         }
